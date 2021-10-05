66de6d62fe4812b1b97881ffdabec2c03f490031
Can Yoldas <can.yoldas0@outlook.com>
2021-10-05 11:36:43 +0300

    first commit

--- Sources/BaseModules/BaseModules.swift
+++ Sources/BaseModules/BaseModules.swift
@@ -1,6 +1,85 @@
-public struct BaseModules {
-    public private(set) var text = "Hello, World!"
+#if canImport(UIKit)
 
-    public init() {
+import UIKit
+
+//MARK: - BaseView
+
+public class BaseView: UIView {
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        addMajorFields()
+        setupViews()
     }
+    
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        addMajorFields()
+        setupViews()
+    }
+    
+    func addMajorFields() {}
+    func setupViews() {}
+    
 }
+
+//MARK: - GenericBaseView
+
+public class GenericBaseView<T>: BaseView {
+    
+    private var data: T?
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    public init(frame: CGRect = .zero, data: T?) {
+        self.data = data
+        super.init(frame: frame)
+        loadDataToView()
+    }
+    
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func loadDataToView() {
+        
+    }
+    
+    func setData(data: T?) {
+        self.data = data
+        loadDataToView()
+    }
+    
+    func returnData() -> T? {
+        return data
+    }
+    
+}
+
+//MARK: - BaseViewController
+
+class BaseViewController<V>: UIViewController {
+    
+    var viewModel: V!
+    
+    convenience init(viewModel: V) {
+        self.init()
+        self.viewModel = viewModel
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        prepareViewControllerSetup()
+    }
+    
+    func prepareViewControllerSetup() {
+        
+        view.backgroundColor = .white
+    }
+    
+}
+
+#endif
