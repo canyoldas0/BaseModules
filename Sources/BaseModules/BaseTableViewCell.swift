9fcce537fec13fc967bf9d075a1ad73db5554a48
Can Yoldas <can.yoldas0@outlook.com>
2021-11-08 22:22:20 +0300

    added new elements

---
+++ Sources/BaseModules/BaseTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  File.swift
+//  
+//
+//  Created by Can Yoldas on 8.11.2021.
+//
+
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+    
+    public override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        addMajorViews()
+        setupView()
+    }
+    
+     required public init?(coder: NSCoder) {
+        super.init(coder: coder)
+        addMajorViews()
+        setupView()
+    }
+    
+    open func setupView() {}
+    open func addMajorViews() {}
+    
+    public static var identifier: String {
+        return String(describing: self)
+    }
+    
+}
---
+++ Sources/BaseModules/GenericDataProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Can Yoldas on 8.11.2021.
+//
+
+import Foundation
+
+
+public protocol GenericDataProtocol {
+    
+}
