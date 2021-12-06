1ebdabf32a1d0db20318ee52766528991ed8852d
Can Yoldas <can.yoldas0@outlook.com>
2021-12-06 21:44:25 +0300

    tableviewcell updated

--- Sources/BaseModules/BaseTableViewCell.swift
+++ Sources/BaseModules/BaseTableViewCell.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class BaseTableViewCell: UITableViewCell {
+open class BaseTableViewCell: UITableViewCell {
     
     public override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
